bc2e4632b3e22b2fa7457fd9db8686271cdcb17a
Matt Harp <wildharpo@yahoo.com>
2024-09-21 19:18:22 -0500

    Adding volunteer and volunteer_repo, plus ignored config files

---
+++ Client/Models/volunteer.py
@@ -0,0 +1,6 @@
+class Volunteer:
+    def __init__(self, first_name:str, last_name:str, phone_number:int, email:str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
