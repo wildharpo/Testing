c9e453d0255c4385b7b1ae7aabbee3ba0fdd08f6
wildharpo <wildharpo@yahoo.com>
2024-10-13 11:34:42 -0500

    Update number_formatter_fixture.py

--- UnitTests/Client/Helpers/number_formatter_fixture.py
+++ UnitTests/Client/Helpers/number_formatter_fixture.py
@@ -6,13 +6,13 @@ from number_formatter import *
 
 class NumberFormatterFixture(unittest.TestCase):
     def test_hyphens_are_removed(self):
-        self.assertEqual(14174397795,get_formatted_number('417-439-7795'))
+        self.assertEqual(14178201234,get_formatted_number('417-820-1234'))
 
     def test_number_beginning_with_one_doesnt_receive_duplicate_one(self):
-        self.assertEqual(14174397795,get_formatted_number('1-417-439-7795'))
+        self.assertEqual(14178201234,get_formatted_number('1-417-820-1234'))
 
     def test_nonnumeric_characters_are_removed(self):
-        self.assertEqual(14174397795,get_formatted_number('4sd17g43hf97p79d5'))
+        self.assertEqual(14178201234,get_formatted_number('4sd17g82hf01p23d4'))
 
     def test_short_number_raises_exception(self):
         with self.assertRaises(ValueError):
